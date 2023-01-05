79057991104860a0d10689ff48a75e5ec8967f29
Bogdan <larionovbogdan1990@gmail.com>
2023-01-05 18:44:30 +0100

    first react app

---
+++ my_app/src/components/App.js
@@ -0,0 +1,14 @@
+import Example from "./Example";
+
+function App() {
+  return (
+    <div>
+      {/* prox */}
+      <Example firs_tname="Oleg" last_name="Petrov" />
+      <Example firs_tname="Anna" last_name="Ivanova" />
+      <Example firs_tname="Bogdan" last_name="Larionov" />
+    </div>
+  );
+}
+
+export default App;
---
+++ my_app/src/components/Example.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Example({ firs_tname, last_name }) {
+  // arguments
+  return (
+    <div>
+      <p>Firstname: {firs_tname}</p>
+      <p>Lastname: {last_name}</p>
+    </div>
+  );
+}
