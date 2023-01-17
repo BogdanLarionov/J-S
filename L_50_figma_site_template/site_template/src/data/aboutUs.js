dcae75804608e3a4613a49850208d6f83c942768
Bogdan <larionovbogdan1990@gmail.com>
2023-01-17 17:25:50 +0100

    array added

---
+++ L_50_figma_site_template/site_template/src/data/aboutUs.js
@@ -0,0 +1,30 @@
+export const aboutUs = [
+  {
+    id: 1,
+    value: 90,
+    title: "Completed big deals",
+    link_title: "All cases",
+    link_url: "examlpe.com",
+  },
+  {
+    id: 2,
+    value: 90,
+    title: "Completed big deals",
+    link_title: "All cases",
+    link_url: "examlpe.com",
+  },
+  {
+    id: 3,
+    value: 90,
+    title: "Completed big deals",
+    link_title: "All cases",
+    link_url: "examlpe.com",
+  },
+  {
+    id: 4,
+    value: 90,
+    title: "Completed big deals",
+    link_title: "All cases",
+    link_url: "examlpe.com",
+  },
+];
