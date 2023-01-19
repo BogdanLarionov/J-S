7dbbe4162308e6f347af2775d535d2af89288363
Bogdan <larionovbogdan1990@gmail.com>
2023-01-19 15:16:45 +0100

    footer added

--- L_50_figma_site_template/site_template/src/components/Footer/index.jsx
+++ L_50_figma_site_template/site_template/src/components/Footer/index.jsx
@@ -3,9 +3,9 @@ import s from "./index.module.css";
 
 export default function Footer() {
   return (
-    <div className={["wrapper", s.footer_block].join(" ")}>
+    <footer className={["wrapper", s.footer].join(" ")}>
       <p>2022 (c) All rights reserved. BuhOne.com Designed by: BuhOne</p>
       <p>Designed by: BuhOne</p>
-    </div>
+    </footer>
   );
 }
