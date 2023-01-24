0bddf0c2ee30767f91e5d837d72b1e9b11418490
Bogdan <larionovbogdan1990@gmail.com>
2023-01-24 13:51:08 +0100

    routing added

--- L_52_routing_practis/routing_practice/src/components/FruitsContainer/index.jsx
+++ L_52_routing_practis/routing_practice/src/components/FruitsContainer/index.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { fruits } from '../../data/fruits'
-import Fruit from '../Fruit'
+import React from "react";
+import { fruits } from "../../data/fruits";
+import Fruit from "../Fruit";
+import s from "./index.module.css";
 
 export default function FruitsContainer() {
   return (
-    <div>
-      {
-        fruits
-          .sort((a, b) => a.price - b.price)
-          .map(el => <Fruit key={el.id} {...el} />)
-      }
+    <div className={s.fruits_container}>
+      {fruits
+        .sort((a, b) => a.price - b.price)
+        .map((el) => (
+          <Fruit key={el.id} {...el} />
+        ))}
     </div>
-  )
+  );
 }
